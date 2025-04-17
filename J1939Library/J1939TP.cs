7c2cc358a588753dce79533482da68d582456a56
FeatherWhite <tomw5823@outlook.com>
2025-04-17 13:13:25 +0800

    Init

---
+++ J1939Library/J1939TP.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace J1939Library
+{
+    public class J1939TP
+    {
+        public delegate bool SendCanFunc(uint arbitrationId, uint channel, byte[] package);
+
+        public SendCanFunc SendCan { get; set; }
+        public uint Channel { get; set; }
+        private Link link = new Link();
+
+        public void ReadDataTransfer(byte SA, byte[] data)
+        {
+            link.ReceiveTP_DT.SequenceNumber = data[0];
+            link.ReceiveTP_DT.FromEcuAddress = SA;
+            byte index = Convert.ToByte(data[0] - 1);
+            for (byte i = 0; i < 8; i++)
+            {
+                //For every package, we send 7 bytes of data where the first byte data[0] is the sequence number
+                link.ReceiveTP_DT.Data[index * 7 + i - 1] = data[i];
+            }
+            //Check if we have completed our message - Return = Not completed
+            if ((link.ReceiveTP_CM.NumberOfPackagesBeingTransmitted != link.ReceiveTP_DT.SequenceNumber) || 
+                (link.ReceiveTP_CM.NumberOfPackagesBeingTransmitted == 0))
+            {
+                if(link.ReceiveTP_CM.ControlByte == (byte)ControlByteCodes.TP_CM_RTS)
+                {
+                    //Send new CTS
+                    link.SendTP_CM.ControlByte = (byte)ControlByteCodes.TP_CM_CTS;
+                    link.SendTP_CM.TotalNumberOfPackagesTransmitted++;
+                    link.SendTP_CM.NextPacketNumberTransmitted++;
+                    SendConnectionManagement(SA);
+                }
+                return;
+            }
+            //Our message are complete - Build it and call it complete_data[total_message_size]
+            uint pgn = link.ReceiveTP_CM.PGNOfThePacketedMessage;
+            ushort totalMessageSize = link.ReceiveTP_CM.TotalMessageSizeBeingTransmitted;
+            byte[] completeData = new byte[Constant.MAX_TP_DT];
+            ushort insertedBytes = 0;
+            for (byte i = 0; i < link.ReceiveTP_DT.SequenceNumber; i++)
+            {
+                for (byte j = 0; j < 7; j++)
+                {
+                    if (insertedBytes < totalMessageSize)
+                    {
+                        completeData[insertedBytes++] = link.ReceiveTP_DT.Data[i * 7 + j];
+                    }
+                }
+            }
+            if(link.ReceiveTP_CM.ControlByte == (byte)ControlByteCodes.TP_CM_RTS)
+            {
+                link.SendTP_CM.ControlByte = (byte)ControlByteCodes.TP_CM_EndOfMsgACK;
+                link.SendTP_CM.TotalNumberOfBytesReceived = link.ReceiveTP_CM.TotalMessageSizeBeingTransmitted;
+                link.SendTP_CM.TotalNumberOfPackagesReceived = link.ReceiveTP_DT.SequenceNumber;
+                SendConnectionManagement(SA);
+            }
+            switch (pgn)
+            {
+                default:
+                    break;
+            }
+            link.ReceiveTP_DT = new TP_DT();
+            link.ReceiveTP_CM = new TP_CM();
+        }
+
+        public void ReadConnectionManagement(byte SA, byte[] data)
+        {
+            //Read the control byte
+            link.ReceiveTP_CM.ControlByte = data[0];
+            //PGN
+            link.ReceiveTP_CM.PGNOfThePacketedMessage = Convert.ToUInt32((data[7] << 16) | (data[6] << 8) | data[5]);
+            //Source address
+            link.ReceiveTP_CM.FromEcuAddress = SA;
+            switch (data[0])
+            {
+                case (byte)ControlByteCodes.TP_CM_RTS:
+                    //Set the RTS values
+                    link.ReceiveTP_CM.TotalMessageSizeBeingTransmitted = Convert.ToUInt16((data[2] << 8) | data[1]);
+                    link.ReceiveTP_CM.NumberOfPackagesBeingTransmitted = Convert.ToByte(data[3]);
+                    //Send CTS
+                    link.SendTP_CM.ControlByte = (byte)ControlByteCodes.TP_CM_CTS;
+                    link.SendTP_CM.TotalNumberOfPackagesTransmitted = 0;
+                    link.SendTP_CM.NextPacketNumberTransmitted = 0;
+                    link.SendTP_CM.PGNOfThePacketedMessage = Convert.ToUInt32((data[7] << 16) | (data[6] << 8) | data[5]);
+                    SendConnectionManagement(SA);
+                    break;
+                case (byte)ControlByteCodes.TP_CM_CTS:
+                    link.ReceiveTP_CM.TotalNumberOfPackagesTransmitted = data[1];
+                    link.ReceiveTP_CM.NextPacketNumberTransmitted = data[2];
+                    SendDataTransfer(SA);
+                    break;
+                case (byte)ControlByteCodes.TP_CM_BAM:
+                    link.ReceiveTP_CM.TotalMessageSizeBeingTransmitted = Convert.ToUInt16((data[2] << 8) | data[1]);
+                    link.ReceiveTP_CM.NumberOfPackagesBeingTransmitted = Convert.ToByte(data[3]);
+                    SendDataTransfer(SA);
+                    break;
+                case (byte)ControlByteCodes.TP_CM_EndOfMsgACK:
+                    link.ReceiveTP_CM.TotalNumberOfBytesReceived = Convert.ToUInt16((data[2] << 8) | data[1]);
+                    link.ReceiveTP_CM.TotalNumberOfPackagesReceived = data[3];
+                break;
+            }
+        }
+
+        public J1939StatusCode SendConnectionManagement(byte DA)
+        {
+            uint ID = Convert.ToUInt32((0x1CEC << 16) | (DA << 8) | link.SendInfo.Address);
+            byte[] data = new byte[8];
+            data[0] = link.SendTP_CM.ControlByte;
+            switch ((ControlByteCodes)data[0])
+            {
+                case ControlByteCodes.TP_CM_RTS:
+                    data[1] = (byte)link.SendTP_CM.TotalMessageSizeBeingTransmitted;
+                    data[2] = (byte)(link.SendTP_CM.TotalMessageSizeBeingTransmitted >> 8);
+                    data[3] = link.SendTP_CM.NumberOfPackagesBeingTransmitted;
+                    data[4] = 0xFF;
+                    break;
+                case ControlByteCodes.TP_CM_CTS:
+                    data[1] = link.SendTP_CM.TotalNumberOfPackagesTransmitted;
+                    data[2] = link.SendTP_CM.NextPacketNumberTransmitted;
+                    data[3] = 0xFF;
+                    data[4] = 0xFF;
+                    break;
+                case ControlByteCodes.TP_CM_BAM:
+                    data[1] = (byte)link.SendTP_CM.TotalMessageSizeBeingTransmitted;
+                    data[2] = (byte)(link.SendTP_CM.TotalMessageSizeBeingTransmitted >> 8);
+                    data[3] = link.SendTP_CM.NumberOfPackagesBeingTransmitted;
+                    data[4] = 0xFF;
+                    break;
+                case ControlByteCodes.TP_CM_EndOfMsgACK:
+                    data[1] = (byte)link.SendTP_CM.TotalNumberOfBytesReceived;
+                    data[2] = (byte)(link.SendTP_CM.TotalNumberOfBytesReceived >> 8);
+                    data[3] = link.SendTP_CM.TotalNumberOfPackagesReceived;
+                    data[4] = 0xFF;
+                    break;
+            }
+            data[5] = (byte)link.SendTP_CM.PGNOfThePacketedMessage;
+            data[6] = (byte)(link.SendTP_CM.PGNOfThePacketedMessage >> 8);
+            data[7] = (byte)(link.SendTP_CM.PGNOfThePacketedMessage >> 16);
+            bool isSuccess = SendCan(ID, Channel, data);
+            J1939StatusCode status = J1939StatusCode.SendOK;
+            if (!isSuccess)
+            {
+                status = J1939StatusCode.SendError;
+            }
+            return status;
+        }
+
+        public J1939StatusCode SendDataTransfer(byte DA)
+        {
+            uint ID = Convert.ToUInt32((0x1CEB << 16) | (DA << 8) | link.SendInfo.Address);
+            byte[] package = new byte[8];
+            ushort bytesSent = 0;
+            J1939StatusCode status = J1939StatusCode.SendOK;
+            switch ((ControlByteCodes)link.ReceiveTP_CM.ControlByte)
+            {
+                case ControlByteCodes.TP_CM_BAM:
+                    {
+                        for (byte i = 1; i <= link.SendTP_CM.NumberOfPackagesBeingTransmitted; i++)
+                        {
+                            package[0] = i;
+                            for (byte j = 0; j < 7; j++)
+                            {
+                                if (bytesSent < link.SendTP_CM.TotalMessageSizeBeingTransmitted)
+                                {
+                                    package[j + 1] = link.SendTP_DT.Data[bytesSent];
+                                }
+                                else
+                                {
+                                    package[j + 1] = 0xFF;
+                                }
+                            }
+                        }
+                        bool isSuccess = SendCan(ID, Channel, package);
+                        Thread.Sleep(100);
+                        if (!isSuccess)
+                        {
+                            status = J1939StatusCode.SendError;
+                            break;
+                        }
+                        break;
+                    }
+
+                case ControlByteCodes.TP_CM_CTS:
+                    {
+                        package[0] = Convert.ToByte(link.ReceiveTP_CM.NextPacketNumberTransmitted + 1);
+                        bytesSent = Convert.ToByte(link.ReceiveTP_CM.TotalNumberOfPackagesTransmitted * 7);
+                        for (byte j = 0; j < 7; j++)
+                        {
+                            if (bytesSent < link.SendTP_CM.TotalMessageSizeBeingTransmitted)
+                            {
+                                package[j + 1] = link.SendTP_DT.Data[bytesSent++];
+                            }
+                            else
+                            {
+                                package[j + 1] = 0xFF;
+                            }
+                        }
+                        bool isSuccess = SendCan(ID, Channel, package);
+                        if (!isSuccess)
+                        {
+                            status = J1939StatusCode.SendError;
+                            break;
+                        }
+                        break;
+                    }
+            }
+            return status;
+        }
+        public J1939StatusCode SendAcknowledgement
+            (byte DA, byte controlByte, byte groupFunctionValue, uint PGNOfRequestedInfo)
+        {
+            uint ID = Convert.ToUInt32((0x18E8 << 16) | (DA << 8) | link.SendInfo.Address);
+            byte[] data = new byte[8];
+            data[0] = controlByte;
+            data[1] = groupFunctionValue; //The cause of the control byte
+            data[2] = 0xFF;
+            data[3] = 0xFF;
+            data[4] = link.SendInfo.Address; //This source address
+            data[5] = (byte)PGNOfRequestedInfo;
+            data[6] = (byte)(PGNOfRequestedInfo >> 8);
+            data[7] = (byte)(PGNOfRequestedInfo >> 16);
+            bool isSuccess = SendCan(ID, Channel, data);
+            J1939StatusCode status = J1939StatusCode.SendOK;
+            if (!isSuccess)
+            {
+                status = J1939StatusCode.SendError;
+            }
+            return status;
+        }
+
+        public J1939StatusCode SendRequest(byte DA,uint PGNCode)
+        {
+            byte[] PGN = new byte[3];
+            PGN[0] = (byte)PGNCode;
+            PGN[1] = (byte)(PGNCode >> 8);
+            PGN[2] = (byte)(PGNCode >> 16);
+            uint ID = Convert.ToUInt32((0x18EA << 16) | (DA << 8) | link.SendInfo.Address);
+            bool isSuccess = SendCan(ID, Channel, PGN);
+            J1939StatusCode status = J1939StatusCode.SendOK;
+            if (!isSuccess)
+            {
+                status = J1939StatusCode.SendError;
+            }
+            return status;
+        }
+
+        public void ReadRequest(byte SA, byte[] data)
+        {
+            uint PGN = Convert.ToUInt32((data[2] << 16) | (data[1] << 8) | data[0]);
+            switch ((PGNCode)PGN)
+            {
+                case PGNCode.PGN_ACKNOWLEDGEMENT:
+                    SendAcknowledgement(SA, (byte)ControlByteCodes.ACKNOWLEDGEMENT_PGN_SUPPORTED, 
+                        (byte)GroupFunctionValueCodes.NORMAL, PGN);
+                    break;
+                case PGNCode.PGN_REQUEST:
+                    SendAcknowledgement(SA, (byte)ControlByteCodes.ACKNOWLEDGEMENT_PGN_SUPPORTED,
+                        (byte)GroupFunctionValueCodes.NORMAL, PGN);
+                    break;
+                case PGNCode.PGN_TP_CM:
+                    SendAcknowledgement(SA, (byte)ControlByteCodes.ACKNOWLEDGEMENT_PGN_SUPPORTED,
+                        (byte)GroupFunctionValueCodes.NORMAL, PGN);
+                    break;
+                case PGNCode.PGN_TP_DT:
+                    SendAcknowledgement(SA, (byte)ControlByteCodes.ACKNOWLEDGEMENT_PGN_SUPPORTED,
+                        (byte)GroupFunctionValueCodes.NORMAL, PGN);
+                    break;
+            }
+        }
+    }
+}
---
+++ J1939Library/Models.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace J1939Library
+{
+    public enum J1939StatusCode : ushort
+    {
+        SendOK = 0x00,
+        SendError = 0x01,
+        SendBusy = 0x02,
+        SendTimeout = 0x03
+    }
+    public enum PGNCode : uint
+    {
+        PGN_ADDRESS_DELETE = 0x000002U, /* NOT SAE J1939 standard. If you find the correct PGN number, please do a pull request */
+        PGN_REQUEST = 0x00EA00U,
+        PGN_ACKNOWLEDGEMENT = 0x00E800U,
+        PGN_TP_CM = 0x00EC00U,
+        PGN_TP_DT = 0x00EB00U,
+        PGN_ADDRESS_CLAIMED = 0x00EE00U,
+        PGN_PROPRIETARY_A = 0x00EF00U,
+        PGN_COMMANDED_ADDRESS = 0x00FED8U,
+        PGN_DM1 = 0x00FECAU,
+        PGN_DM2 = 0x00FECBU,
+        PGN_DM3 = 0x00FECCU,
+        PGN_DM14 = 0x00D900U,
+        PGN_DM15 = 0x00D800U,
+        PGN_DM16 = 0x00D700U,
+        PGN_SOFTWARE_IDENTIFICATION = 0x00FEDAU,
+        PGN_ECU_IDENTIFICATION = 0x00FDC5U,
+        PGN_COMPONENT_IDENTIFICATION = 0x00FEEBU,
+        PGN_AUXILIARY_VALVE_ESTIMATED_FLOW_0 = 0x00FE10U,
+        PGN_AUXILIARY_VALVE_ESTIMATED_FLOW_1 = 0x00FE11U,
+        PGN_AUXILIARY_VALVE_ESTIMATED_FLOW_2 = 0x00FE12U,
+        PGN_AUXILIARY_VALVE_ESTIMATED_FLOW_3 = 0x00FE13U,
+        PGN_AUXILIARY_VALVE_ESTIMATED_FLOW_4 = 0x00FE14U,
+        PGN_AUXILIARY_VALVE_ESTIMATED_FLOW_5 = 0x00FE15U,
+        PGN_AUXILIARY_VALVE_ESTIMATED_FLOW_6 = 0x00FE16U,
+        PGN_AUXILIARY_VALVE_ESTIMATED_FLOW_7 = 0x00FE17U,
+        PGN_AUXILIARY_VALVE_ESTIMATED_FLOW_8 = 0x00FE18U,
+        PGN_AUXILIARY_VALVE_ESTIMATED_FLOW_9 = 0x00FE19U,
+        PGN_AUXILIARY_VALVE_ESTIMATED_FLOW_10 = 0x00FE1AU,
+        PGN_AUXILIARY_VALVE_ESTIMATED_FLOW_11 = 0x00FE1BU,
+        PGN_AUXILIARY_VALVE_ESTIMATED_FLOW_12 = 0x00FE1CU,
+        PGN_AUXILIARY_VALVE_ESTIMATED_FLOW_13 = 0x00FE1DU,
+        PGN_AUXILIARY_VALVE_ESTIMATED_FLOW_14 = 0x00FE1EU,
+        PGN_AUXILIARY_VALVE_ESTIMATED_FLOW_15 = 0x00FE1FU,
+        PGN_AUXILIARY_VALVE_MEASURED_POSITION_0 = 0x00FF20U,
+        PGN_AUXILIARY_VALVE_MEASURED_POSITION_1 = 0x00FF21U,
+        PGN_AUXILIARY_VALVE_MEASURED_POSITION_2 = 0x00FF22U,
+        PGN_AUXILIARY_VALVE_MEASURED_POSITION_3 = 0x00FF23U,
+        PGN_AUXILIARY_VALVE_MEASURED_POSITION_4 = 0x00FF24U,
+        PGN_AUXILIARY_VALVE_MEASURED_POSITION_5 = 0x00FF25U,
+        PGN_AUXILIARY_VALVE_MEASURED_POSITION_6 = 0x00FF26U,
+        PGN_AUXILIARY_VALVE_MEASURED_POSITION_7 = 0x00FF27U,
+        PGN_AUXILIARY_VALVE_MEASURED_POSITION_8 = 0x00FF28U,
+        PGN_AUXILIARY_VALVE_MEASURED_POSITION_9 = 0x00FF29U,
+        PGN_AUXILIARY_VALVE_MEASURED_POSITION_10 = 0x00FF2AU,
+        PGN_AUXILIARY_VALVE_MEASURED_POSITION_11 = 0x00FF2BU,
+        PGN_AUXILIARY_VALVE_MEASURED_POSITION_12 = 0x00FF2CU,
+        PGN_AUXILIARY_VALVE_MEASURED_POSITION_13 = 0x00FF2DU,
+        PGN_AUXILIARY_VALVE_MEASURED_POSITION_14 = 0x00FF2EU,
+        PGN_AUXILIARY_VALVE_MEASURED_POSITION_15 = 0x00FF2FU,
+        PGN_AUXILIARY_VALVE_COMMAND_0 = 0x00FE30U,
+        PGN_AUXILIARY_VALVE_COMMAND_1 = 0x00FE31U,
+        PGN_AUXILIARY_VALVE_COMMAND_2 = 0x00FE32U,
+        PGN_AUXILIARY_VALVE_COMMAND_3 = 0x00FE33U,
+        PGN_AUXILIARY_VALVE_COMMAND_4 = 0x00FE34U,
+        PGN_AUXILIARY_VALVE_COMMAND_5 = 0x00FE35U,
+        PGN_AUXILIARY_VALVE_COMMAND_6 = 0x00FE36U,
+        PGN_AUXILIARY_VALVE_COMMAND_7 = 0x00FE37U,
+        PGN_AUXILIARY_VALVE_COMMAND_8 = 0x00FE38U,
+        PGN_AUXILIARY_VALVE_COMMAND_9 = 0x00FE39U,
+        PGN_AUXILIARY_VALVE_COMMAND_10 = 0x00FE3AU,
+        PGN_AUXILIARY_VALVE_COMMAND_11 = 0x00FE3BU,
+        PGN_AUXILIARY_VALVE_COMMAND_12 = 0x00FE3CU,
+        PGN_AUXILIARY_VALVE_COMMAND_13 = 0x00FE3DU,
+        PGN_AUXILIARY_VALVE_COMMAND_14 = 0x00FE3EU,
+        PGN_AUXILIARY_VALVE_COMMAND_15 = 0x00FE3FU,
+        PGN_GENERAL_PURPOSE_VALVE_ESTIMATED_FLOW = 0x00C600U,
+        PGN_ENGINE_HOURS_65253 = 0x00FE3FU,
+        PGN_ENGINE_TEMPERATURE_1_65262 = 0x00FEEEU,
+        PGN_VEHICLE_ELECTRICAL_POWER_1_65271 = 0x00FEF7U,
+        PGN_ELECTRONIC_ENGINE_CONTROLLER_1_61444 = 0x00F004U,
+        PGN_COLD_START_AIDS_64966 = 0x00FDC6U,
+        PGN_FUEL_CONSUMPTION_65257 = 0x00FEE9U,
+        PGN_FUEL_ECONOMY_65266 = 0x00FEF2U,
+        PGN_ENGINE_FLUIDS_LEVEL_PRESSURE_1_65263 = 0x00FEEFU,
+        PGN_ELECTRONIC_ENGINE_CONTROLLER_2_61443 = 0x00F003U,
+        PGN_AMBIENT_CONDITIONS_65269 = 0x00FEF5U,
+        PGN_ENGINE_FUEL_LUBE_SYSTEMS_65130 = 0x00FE6AU,
+        PGN_AUXILIARY_ANALOG_INFORMATION_65164 = 0x00FE8CU,
+        PGN_AFTERTREATMENT_1_DEF_TANK_1_65110 = 0x00FE56U,
+        PGN_SHUTDOWN_65252 = 0x00FEE4U,
+        PGN_ELECTRONIC_ENGINE_CONTROLLER_3_65247 = 0x00FEDFU,
+        PGN_ENGINE_FLUIDS_LEVEL_PRESSURE_12_64735 = 0x00FCDFU,
+        PGN_INTAKE_MANIFOLD_INFO_1_65190 = 0x00FEA6U,
+        PGN_DASH_DISPLAY_65276 = 0x00FEFCU,
+        PGN_DIRECT_LAMP_CONTROL_COMMAND_1_64775 = 0x00FD07U,
+        PGN_TORQUE_SPEED_CONTROL_1_0 = 0x000000U,
+        PGN_ELECTRONIC_BRAKE_CONTROLLER_1_61441 = 0x00F001U,
+        PGN_PROPRIETARY_B_START = 0x00FF00U,
+        PGN_PROPRIETARY_B_END = 0x00FFFFU,
+        // Same as proprietary B but with DP (data page = 1)
+        PGN_PROPRIETARY_B2_START = 0x01FF00U, /* This range is not officially support by the standard */
+        PGN_PROPRIETARY_B2_END = 0x01FFFFU   /* but it is very commonly used, so it's supported here */
+
+        /* Add more here */
+    }
+
+    public class Constant
+    {
+        public const uint MAX_TP_DT = 1785;
+        public const uint MAX_IDENTIFICATION = 30;
+        public const uint MAX_DM_FIELD = 10;
+        public const uint MAX_PROPRIETARY_A = 15;
+        public const uint MAX_PROPRIETARY_B = 60;
+        public const uint MAX_PROPRIETARY_B_PGNS = 2;
+    }
+
+    /// <summary>
+    /// PGN: 0x00FEDA - Storing the software identification from the reading process
+    /// </summary>
+    public class SoftwareIdentification
+    {
+        public byte number_of_fields; /* How many numbers contains in the identifications array */
+        public byte[] identifications = new byte[Constant.MAX_IDENTIFICATION];    /* This can be for example ASCII */
+        public byte from_ecu_address;	/* From which ECU came this message */
+    }
+
+    /// <summary>
+    /// PGN: 0x00FDC5 - Storing the ECU identification from the reading process
+    /// </summary>
+    public class ECUIdentification
+    {
+        public byte length_of_each_field;                   /* The real length of the fields - Not part of J1939 standard, only for the user */
+        public byte[] ecu_part_number = new byte[Constant.MAX_IDENTIFICATION];    /* ASCII field */
+        public byte[] ecu_serial_number = new byte[Constant.MAX_IDENTIFICATION];  /* ASCII field */
+        public byte[] ecu_location = new byte[Constant.MAX_IDENTIFICATION];       /* ASCII field */
+        public byte[] ecu_type = new byte[Constant.MAX_IDENTIFICATION];           /* ASCII field */
+        public byte from_ecu_address;                       /* From which ECU came this message */
+    };
+
+    /* PGN: 0x00FEEB - Storing the component identification from the reading process */
+
+    public class ComponentIdentification
+    {
+        public byte length_of_each_field { get; set; }                   /* The real length of the fields - Not part of J1939 standard, only for the user  */
+        public byte[] component_product_date { get; set; } = new byte[Constant.MAX_IDENTIFICATION]; /* ASCII field */
+        public byte[] component_model_name { get; set; } = new byte[Constant.MAX_IDENTIFICATION];   /* ASCII field */
+        public byte[] component_serial_number { get; set; } = new byte[Constant.MAX_IDENTIFICATION];/* ASCII field */
+        public byte[] component_unit_name { get; set; } = new byte[Constant.MAX_IDENTIFICATION];    /* ASCII field */
+        public byte from_ecu_address { get; set; }                      /* From which ECU came this message */
+    };
+
+    /* PGN: 0x00EE00 - Storing the Address claimed from the reading process */
+
+    public class Name
+    {
+        public uint identity_number { get; set; }                       /* Specify the ECU serial ID - 0 to 2097151 */
+        public ushort manufacturer_code { get; set; }                     /* Specify the ECU manufacturer code - 0 to 2047 */
+        public byte function_instance { get; set; }                      /* Specify the ECU function number - 0 to 31 */
+        public byte ECU_instance { get; set; }                           /* Specify the ECU number - 0 to 7 */
+        public byte function { get; set; }                               /* Specify the ECU function - 0 to 255 */
+        public byte vehicle_system { get; set; }                         /* Specify the type of vehicle where ECU is located - 0 to 127 */
+        public byte arbitrary_address_capable { get; set; }              /* Specify if the ECU have right to change address if addresses conflicts - 0 to 1 */
+        public byte industry_group { get; set; }                         /* Specify the group where this ECU is located - 0 to 7 */
+        public byte vehicle_system_instance { get; set; }                /* Specify the vehicle system number - 0 to 15 */
+        public byte from_ecu_address { get; set; }                       /* From which ECU came this message */
+    };
+
+    public class Identifications
+    {
+        public SoftwareIdentification SoftwareIdentification { get; set; } = new SoftwareIdentification();
+        public ECUIdentification EcuIdentification { get; set; } = new ECUIdentification();
+        public ComponentIdentification ComponentIdentification { get; set; } = new ComponentIdentification();
+    }
+
+    public class SenderInfo
+    {
+        public Name Name { get; set; } = new Name();
+        public byte Address { get; set; }
+        public Identifications Identifications { get; set; } = new Identifications();
+    }
+
+    /// <summary>
+    /// PGN: 0x00EC00 - Storing the Transport Protocol Connection Management from the reading process
+    /// </summary>
+    public class TP_CM
+    {
+        /* General */
+        public byte ControlByte { get; set; } // What type of message are we going to send
+
+        /* RTS */
+        public ushort TotalMessageSizeBeingTransmitted { get; set; } // Total bytes our complete message includes - 9 to 1785
+        public byte NumberOfPackagesBeingTransmitted { get; set; } // How many times we are going to send packages via TP_DT - 2 to 224
+
+        /* CTS */
+        public byte TotalNumberOfPackagesTransmitted { get; set; } // Total packages we have received
+        public byte NextPacketNumberTransmitted { get; set; } // Next packet number we want to have from the transmitter
+
+        /* EOM */
+        public ushort TotalNumberOfBytesReceived { get; set; } // Total bytes we are have got
+        public byte TotalNumberOfPackagesReceived { get; set; } // Total number of packages received
+
+        /* Abort */
+        public byte ConnectingAbortReason { get; set; } // Given a message about the reason
+
+        /* General */
+        public uint PGNOfThePacketedMessage { get; set; } // Our message is going to activate a PGN
+        public byte FromEcuAddress { get; set; } // From which ECU came this message
+    }
+
+    /// <summary>
+    /// Control bytes enums
+    /// </summary>
+    public enum ControlByteCodes : byte
+    {
+        TP_CM_ABORT = 0xFF,
+        TP_CM_BAM = 0x20,
+        TP_CM_EndOfMsgACK = 0x13,
+        TP_CM_CTS = 0x11,
+        TP_CM_RTS = 0x10,
+        ACKNOWLEDGEMENT_PGN_SUPPORTED = 0x0,
+        ACKNOWLEDGEMENT_PGN_NOT_SUPPORTED = 0x1,
+        ACKNOWLEDGEMENT_PGN_ACCESS_DENIED = 0x2,
+        ACKNOWLEDGEMENT_PGN_BUSY = 0x3
+        // Add more control bytes here
+    }
+
+    /// <summary>
+    /// PGN: 0x00ED00 - Storing the Transport Protocol Data Transfer from the reading process
+    /// </summary>
+    public class TP_DT
+    {
+        public byte SequenceNumber { get; set; } // When this sequence number is the same as number_of_packages from TP_CM, then we have our complete message
+        public byte[] Data { get; set; } = new byte[Constant.MAX_TP_DT]; // This is the collected data we are going to send. Also we are using this as a filler
+        public byte FromEcuAddress { get; set; } // From which ECU came this message
+    }
+
+    ///<summary>
+    ///This struct is used for handling J1939 information
+    ///</summary>
+    public class Link
+    {
+        /// <summary>
+        /// For ID information about this ECU - SAE J1939
+        /// </summary>
+        public SenderInfo SendInfo { get; set; } = new SenderInfo();
+        public TP_DT ReceiveTP_DT { get; set; } = new TP_DT();
+        public TP_CM ReceiveTP_CM { get; set; } = new TP_CM();
+        public TP_DT SendTP_DT { get; set; } = new TP_DT();
+        public TP_CM SendTP_CM { get; set; } = new TP_CM();
+    }
+    /* Enums for the acknowledgements */
+    public enum GroupFunctionValueCodes : byte
+    {
+        NORMAL = 0x0,
+        CANNOT_MAINTAIN_ANOTHER_CONNECTION = 0x1,
+        LACKING_NECESSARY_RESOURCES = 0x2,
+        ABORT_TIME_OUT = 0x3,
+        NO_CAUSE = 0xFF
+    }
+}
