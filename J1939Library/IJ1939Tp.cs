49468c0c18f08b3232ab0543b24ad84fb7b0ceed
FeatherWhite <tomw5823@outlook.com>
2025-05-26 08:27:45 +0800

    IJ1939Tp update J1939 Tp Interface

--- J1939Library/IJ1939Tp.cs
+++ J1939Library/IJ1939Tp.cs
@@ -6,8 +6,10 @@ namespace Triumph.J1939
 {
     public interface IJ1939Tp
     {
-        public void Poll();
+        public J1939TpStatus Job();
         public bool Send(byte dataPage, byte pduFormat, 
             byte priority, byte[] data);
+        public void Receive();
+        public byte[] GetRevcData();
     }
 }
